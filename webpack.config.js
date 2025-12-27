89edc59cbe73ef1392070c6575a961ff0616a081
Antoine GANNAT <antoine.gannat@gmail.com>
2025-12-27 17:02:03 +0100

    Add GH actions

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
     clean: true,
+    publicPath: process.env.NODE_ENV === 'production' ? '/budget-viewer/' : '/',
   },
   module: {
     rules: [
